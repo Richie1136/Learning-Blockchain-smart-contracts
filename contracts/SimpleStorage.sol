d956b5077b5f6ac543b7d605a4872a412402d579
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-03 09:10:44 -0400

    Learning about 4 basic types of solidity

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,4 +2,15 @@
 
 pragma solidity ^0.8.7;
 
-contract SimpleStorage {}
+// Basic Solidity Types
+
+// boolean - True / False
+// uint - Unsigned integer, which means its going to be a whole number, that isn't
+// positive or negative, its just positive
+
+// int - Positive or Negative whole number
+// address - An address
+
+contract SimpleStorage {
+    // boolean, uint, int, address, bytes
+}
