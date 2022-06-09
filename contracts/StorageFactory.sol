9bc12d492fe49c6f96cabbc97df8670ade3a9e59
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-09 09:08:30 -0400

    Created another function which is going to call the store function from the SimpleStorage file from inside of the StorageFactory

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -13,4 +13,13 @@ contract StorageFactory {
         // pushing the simpleStorage into the array
         simpleStorageArray.push(simpleStorage);
     }
+
+    function storagefactoryStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        // Address
+        // ABI - Application Binary Interface. Tells you all of the inputs and
+        // outputs and everything you could do with the contract
+    }
 }
