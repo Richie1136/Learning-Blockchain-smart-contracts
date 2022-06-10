234ce96400074cff045b91ee981b5c83b3a2b633
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-10 09:43:28 -0400

    Get the value that somebody is sending and that value has to be greater then 1 Ether otherwise a error message will display

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -34,12 +34,19 @@
 // Withdraw funds
 // Set a minimum funding value in USD
 
+// Both wallets and contracts can hold native blockchain token like Ethereum.
+
 pragma solidity ^0.8.7;
 
 contract FundMe {
-    function fund() public {
+    function fund() public payable {
+        // Smart contracts can hold funds just like how wallets can
         // Want to be able to set a minium fund amount in USD
         // 1. How do we send ETH to this contract?
+
+        // Using msg.value to get how much value somebody is sending
+
+        require(msg.value > 1e18, "Didn't send enough"); // 1e18 is equal to 1 * 10 ** 18
     }
 
     // function withdraw() {}
