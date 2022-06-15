2fde4adf263d2c7b5a31490de2fe3f288f481068
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 16:19:55 -0400

    Created a getConversion function to convert the msg.value from ethereum to USD

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -76,8 +76,10 @@
 
 pragma solidity ^0.8.7;
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
-    uint256 public minUSD = 50;
+    uint256 public minUSD = 50 * 1e18;
 
     function fund() public payable {
         // Smart contracts can hold funds just like how wallets can
@@ -85,7 +87,38 @@ contract FundMe {
         // 1. How do we send ETH to this contract?
 
         // Using msg.value to get how much value somebody is sending
-        require(msg.value >= minUSD, "Didn't send enough"); // 1e18 is equal to 1 * 10 ** 18
+        require(getConversion(msg.value) >= minUSD, "Didn't send enough"); // 1e18 is equal to 1 * 10 ** 18
+        // 18 Decimal places becuase 1 ether === 1000000000000000000(18 0s)
+    }
+
+    function getPrice() public view returns (uint256) {
+        // Getting the price of ethereum in terms of USD, so we can convert our message.value
+        // to USD
+        // ABI
+        // Address //0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        // Price feed object
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        // price is an int because a price could be positive or negative
+        (, int256 answer, , ,) = priceFeed.latestRoundData();
+        // ETH in terms of USD
+        return uint256(answer * 1e10); // 1**10
+        // converting the price value from a int256 to a uint256
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+
+        return priceFeed.version();
+    }
+
+    function getConversion(uint256 ethAmount) public view returns (uint256) {
+        // Converting the msg.value from ethereum to terms of USD
+        uint256 ethPrice = getPrice();
+        // 3000_000000000000000000 3 thousand dollars ETH / USD Price
+        // We send 1_000000000000000000 ETH
+        // Multiple the 2 numbers above and then divide 1_000000000000000000
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
     }
 
     // function withdraw() {}
