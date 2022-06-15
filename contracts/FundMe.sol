f3f4910c7ba5680afdf0dbc58520685052ff436a
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-06-15 16:22:35 -0400

    Created an array of funders which i made public and then i am pushing the msg sender into the array

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -81,6 +81,8 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe {
     uint256 public minUSD = 50 * 1e18;
 
+    address[] public funders;
+
     function fund() public payable {
         // Smart contracts can hold funds just like how wallets can
         // Want to be able to set a minium fund amount in USD
@@ -89,6 +91,7 @@ contract FundMe {
         // Using msg.value to get how much value somebody is sending
         require(getConversion(msg.value) >= minUSD, "Didn't send enough"); // 1e18 is equal to 1 * 10 ** 18
         // 18 Decimal places becuase 1 ether === 1000000000000000000(18 0s)
+        funders.push(msg.sender);
     }
 
     function getPrice() public view returns (uint256) {
@@ -97,16 +100,20 @@ contract FundMe {
         // ABI
         // Address //0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         // Price feed object
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
         // price is an int because a price could be positive or negative
-        (, int256 answer, , ,) = priceFeed.latestRoundData();
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
         // ETH in terms of USD
         return uint256(answer * 1e10); // 1**10
         // converting the price value from a int256 to a uint256
     }
 
     function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        );
 
         return priceFeed.version();
     }
