345956bc47fe0ae19367751ac57b3117490e9aa9
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-08-18 16:20:27 -0400

    Created wallet using the encrypted key

--- contracts/etheres-simple-storage/deploy.js
+++ contracts/etheres-simple-storage/deploy.js
@@ -52,7 +52,10 @@ const main = async () => {
   // compile them separately
   // http://127.0.0.1:7545
   const provider = new ethers.providers.JsonRpcProvider(process.env.RPC_URL)
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider)
+  const encryptedJson = fs.readFileSync('./encryptedKey.json', "utf-8")
+  let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY)
+  wallet = await wallet.connect(provider)
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf-8")
   const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin", "utf-8")
   // A contract factory is just an object that you can use to deploy contracts.
